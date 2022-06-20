62b4915f9f472de3456b5a86beb9559e23069e1f
Matheus do Carmo Cardoso <matheus.cardoso@faegsenar.local.br>
2022-06-19 21:44:20 -0300

    avançando no estudos de vuejs

---
+++ 08-basic-avanced/vagas-front/src/components/comuns/VagaComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <div class="card-header bg-dark text-white">{{ tituloVagaTeste }}</div>
+        <div class="card-body">
+        <p>{{ descricaoVaga }}</p>
+        </div>
+        <div class="card-footer">
+        <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VagaComponent',
+    props: ['tituloVagaTeste', 'descricaoVaga', 'salario', 'modalidade', 'tipo', 'publicacao']
+}
+</script>
